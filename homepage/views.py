c683a0e8df08464d7ead878c3d56718811300a26
urlordjames <urlordjames@gmail.com>
2020-05-22 12:06:43 -0400

    fixed user owning of songs

--- homepage/views.py
+++ homepage/views.py
@@ -16,24 +16,30 @@ def loginpage(request):
         loginform = forms.LoginForm(request.POST)
         if not loginform.is_valid():
             messages.error(request, "login form invalid")
-            return redirect("/")
+            return redirect("/login/")
         formdata = loginform.cleaned_data
         user = authenticate(request, username=formdata["username"], password=formdata["password"])
         if user != None:
             login(request, user)
             messages.success(request, "you have successfully logged in!")
+            return redirect("/")
         else:
             messages.error(request, "authentication failed")
-        return redirect("/")
+        return redirect("/login/")
 
 @csrf_protect
 def uploadpage(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "you are not logged in")
+        return redirect("/login/")
     if request.method != "POST":
         return render(request, "form.html", {"form": forms.SongUpload, "destination": "/upload/", "action": "upload"})
     else:
         uploadform = forms.SongUpload(request.POST, request.FILES)
         if uploadform.is_valid:
-            uploadform.save()
+            data = uploadform.save(commit=False)
+            data.uploader = request.user
+            data.save()
             messages.success(request, "song successfully uploaded")
             return redirect("/")
         messages.error(request, "upload form invalid")
