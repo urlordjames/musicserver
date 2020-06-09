63b6c03ac3e4cb062df72e6b59d198468a9ba2dd
urlordjames <urlordjames@gmail.com>
2020-06-08 21:06:26 -0400

    added alphanumeric validator for media title

--- homepage/models.py
+++ homepage/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+
+def isfilesafe(string):
+    if not string.isalpha():
+        raise ValidationError("string is not alphanumeric")
 
 # Create your models here.
 
 class Song(models.Model):
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, null=False, blank=False, validators=[isfilesafe])
