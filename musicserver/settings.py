2b0878cd6074af7891388e3cebbd20d6236d84c8
urlordjames <urlordjames@gmail.com>
2020-06-18 18:19:27 -0400

    changed static path

--- musicserver/settings.py
+++ musicserver/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/deployproxy/staticfiles'
 STATIC_ROOT = 'static'
