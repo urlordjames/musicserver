f36414755431315cd561c7e6601b28755ccf6b2e
urlordjames <urlordjames@gmail.com>
2020-06-20 20:20:57 -0400

    fixed mobile css stuff

--- musicserver/settings.py
+++ musicserver/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'musicserver.urls'
